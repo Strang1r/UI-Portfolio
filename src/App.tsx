1578ec6cb53b037c75b4368c4f7659c34daa28db
Alex Ma <YunMa@clarku.edu>
2026-02-01 16:36:24 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <div className="app">
+      <div className="header">
+        <img src="/pro.png" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default App
